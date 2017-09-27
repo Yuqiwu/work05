56c9e4a2eb86e909d22419c83e63b5bbb7d55693
Yuqiwu <ywu@stuy.edu>
2017-09-26 21:24:14 -0400

    finished!

--- work03.py
+++ work03.py
@@ -6,17 +6,7 @@ with open('occupations.csv') as csvfile:
     d = {}
     c = 0
     for row in reader:
-        d[ (c, c + float (row['Percentage']))  ] =  row['Job Class']
-        c = c + float(row['Percentage'])
+        d[ c ] =  [ row['Job Class'], row['Percentage'], row['Link'] ]
+        c = c + 1
 
-#for job in d:
-#    print (job , d[job])    
-
-def randomPick(data):
-    num = random.random() * 99.8
-    for job in data:
-        if num > job[0] and num <= job[1]:
-            return d[job]
-            
-print randomPick(d)
-    
+print d
--- work05.py
+++ work05.py
@@ -1,20 +1,34 @@
 from flask import Flask, render_template
 import csv
+import random
+		
+my_app = Flask(__name__)
+
+@my_app.route('/')
+def root():
+	return "Hi this is Yu Qi and Michela's page"
 
 with open('occupations.csv') as csvfile:
     reader = csv.DictReader(csvfile)
     d = {}
+    c = 0
     for row in reader:
-        d[ row['Job Class'] ]= row['Percentage']
+        d[ c ] =  [ row['Job Class'], row['Percentage'], row['Link'] ]
+        c = c + 1
 
-my_app = Flask(__name__)
-
+def randomPick(data):
+    num = random.randint(0,21)
+    for job in data:
+        if num == job:
+            return d[job]
+	
 @my_app.route('/occupations')
 def occupations():
-	#{% block table %}
-	
-	#{% endblock %}
-	return render_template('basic.html')
+	obj = randomPick(d)
+	occ = obj[0]
+	pen = obj[1]
+	lin = obj[2]
+	return render_template('basic.html', occupation = occ, percent = pen, link = lin)
 
 if __name__ == '__main__':
 	my_app.debug = True
